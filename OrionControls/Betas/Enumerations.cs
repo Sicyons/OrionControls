65b5a1432970f04be59c2551f69d26f0e83821c3
Franck <franck.deniau@auxitec.fr>
2018-04-09 14:38:14 +0200

    Adding OrionButton

---
+++ OrionControls/Betas/Enumerations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrionControls
+{
+    #region Enumerations
+    public enum Backgroundtypes
+    {
+        None = 0,
+        BeveledRectangle = 1,
+        Circle = 2,
+        Diamond = 3,
+        Rectangle = 4
+    }
+    #endregion
+}
---
+++ OrionControls/Betas/OrionButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OrionControls
+{
+    /// <summary>
+    /// Logique d'interaction pour OrionButton.xaml
+    /// </summary>
+    public partial class OrionButton : Button
+    {
+        #region Dependency properties
+        public static readonly DependencyProperty ShadowDepthProperty = DependencyProperty.Register("ShadowDepth", typeof(Int32), typeof(OrionButton), new FrameworkPropertyMetadata(10));
+        public static readonly DependencyProperty BackgroundTypeProperty = DependencyProperty.Register("TypeFond", typeof(Backgroundtypes), typeof(OrionButton), new FrameworkPropertyMetadata(Backgroundtypes.None));
+
+        public Int32 ShadowDepth
+        {
+            get { return (Int32)GetValue(ShadowDepthProperty); }
+            set { SetValue(ShadowDepthProperty, value); }
+        }
+        public Backgroundtypes BackgroundType
+        {
+            get { return (Backgroundtypes)GetValue(BackgroundTypeProperty); }
+            set { SetValue(BackgroundTypeProperty, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public OrionButton()
+        {
+            InitializeComponent();
+        }// OrionButton()
+        #endregion
+    }
+}
